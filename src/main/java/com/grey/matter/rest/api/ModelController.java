20ccec24b87f530c4e86d06d27c6da37ecb60bd5
Moshlehuddin <apu@stibodx.com>
2021-09-23 23:54:52 +0600

    - Post model to api added and corresponding test added.

--- src/main/java/com/grey/matter/rest/api/ModelController.java
+++ src/main/java/com/grey/matter/rest/api/ModelController.java
@@ -1,16 +1,38 @@
 package com.grey.matter.rest.api;
 
 import com.grey.matter.rest.model.ModelDescriptor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 public class ModelController {
+  private final List<ModelDescriptor> models = new ArrayList<>();
+
+  public ModelController() {
+    var model = ModelDescriptor.builder().name("watch").label("Watch").build();
+    models.add(model);
+  }
+
   @GetMapping("/models")
   List<ModelDescriptor> getModels() {
-    var model = ModelDescriptor.builder().name("watch").label("Watch").build();
-    return List.of(model);
+    return models;
+  }
+
+  @PostMapping(path = "/models", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+  ResponseEntity<ModelDescriptor> addModel(@Validated @RequestBody ModelDescriptor pModelDescriptor) {
+    if (models.stream().anyMatch(model -> model.getName().equalsIgnoreCase(pModelDescriptor.getName()))) {
+      return ResponseEntity.badRequest().build();
+    }
+    models.add(pModelDescriptor);
+    return ResponseEntity.of(Optional.of(pModelDescriptor));
   }
 }
--- src/test/java/com/grey/matter/rest/RestApplicationTests.java
+++ src/test/java/com/grey/matter/rest/RestApplicationTests.java
@@ -1,12 +1,19 @@
 package com.grey.matter.rest;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grey.matter.rest.model.ModelDescriptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +34,30 @@ class RestApplicationTests {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$").isArray())
         .andExpect(jsonPath("$[0].name").value("watch"));
+  }
+
+  @Test
+  void postModel() throws Exception {
+    var xyz = ModelDescriptor.builder().name("xyz").label("XYZ").build();
+    mMockMvc.perform(post("/models")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(convertObjectToJsonBytes(xyz)))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void failedPostModel() throws Exception {
+    var xyz = ModelDescriptor.builder().name("watch").label("Watch").build();
+    mMockMvc.perform(post("/models")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(xyz)))
+            .andExpect(status().is4xxClientError());
+  }
+
+  public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    return mapper.writeValueAsBytes(object);
   }
 
 }
